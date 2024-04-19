11a1b17726de7546c2f3e8df5cde11bb3fa74be8
Your Name <you@example.com>
2024-04-19 03:18:17 +0000

    add invoices

--- next.config.js
+++ next.config.js
@@ -2,3 +2,49 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+// module.exports = {
+//     devServer: {
+//       proxy: {
+//         '/api': {
+//           target: 'http://192.168.1.21:3000',
+//           changeOrigin: true,
+//           pathRewrite: {
+//             '^/api': '',
+//           },
+//         },
+//       },
+//     },
+//   }
+
+
+
+// const securityHeaders = [{
+//     key: 'X-DNS-Prefetch-Control',
+//     value: 'on'
+//   },{
+//     key: 'X-Forwarded-Host',
+//     value: '0.0.0.0:3000'
+//   },{
+//     key: 'X-Forwarded-Host',
+//     value: '*'
+//   },{
+//     key: 'x-forwarded-host',
+//     value: '0.0.0.0:3000'
+//   },{
+//     key: 'x-forwarded-host',
+//     value: '*'
+//   }]
+
+//   //x-forwarded-host
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         // Apply these headers to all routes in your application.
+//         source: '/(.*)',
+//         headers: securityHeaders,
+//       },
+//     ]
+//   },
+// }
